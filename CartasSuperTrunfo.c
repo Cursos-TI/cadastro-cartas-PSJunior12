e68856bb2019ee1ce321520bfb190d969fffb7ca
PSJunior12 <psthebest_thebest@hotmail.com>
2026-07-29 04:15:41 +0000

    Fiz o nível mestre acrescentando a comparação das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,7 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
      //Informações de variáveis da Cidade 1
-  int populacao1, turismo1 = 50;  
+  unsigned long int populacao1, turismo1 = 50;  
   float area1, pib1 = 133.75;
   char estado1 = 'A';
   char codigo1[5], cidade1[50] = "Serra";
@@ -16,7 +16,7 @@ int main() {
   float pibpercapita1 = 1.1;
 
      //Informações de variáveis da Cidade 2
-  int populacao2, turismo2 = 30;  
+  unsigned long int populacao2, turismo2 = 30;  
   float area2, pib2 = 233.75;
   char estado2 = 'H';
   char codigo2[5], cidade2[50] = "Rio";
@@ -47,11 +47,21 @@ int main() {
   printf("Digite a quantidade de pontos turísticos: ");
   scanf(" %d" ,&turismo1);
 
-//Calcular a Densidade Populacional = População / Área 
- densidade1 = populacao1 / area1;  
+  //Calcular a Densidade Populacional = População / Área 
+  densidade1 = populacao1 / area1;  
 
-//Calcular Pib per Capita = PIB / população
- pibpercapita1 = pib1 / populacao1; 
+  //Calcular Pib per Capita = PIB / população
+  pibpercapita1 = pib1 / populacao1; 
+
+
+  //Calcular o Super Poder somando todos atributos numéricos
+  float superpoder1 = 
+    (float)populacao1 + 
+    area1 + 
+    pib1 + 
+    turismo1 + 
+    pibpercapita1 + 
+    ((float)1 / densidade1);
 
     //Dados da Cidade 2
 
@@ -65,7 +75,7 @@ int main() {
   scanf("%s" ,cidade2);
 
   printf("Digite a população da cidade: ");
-  scanf("%d" ,&populacao2);
+  scanf(" %d" ,&populacao2);
 
   printf("Digite a área da cidade em km²:");
   scanf(" %f" ,&area2);
@@ -77,11 +87,21 @@ int main() {
   scanf(" %d" ,&turismo2);
 
 
-//Calcular a Densidade Populacional = População / Área 
- densidade2 = populacao2 / area2;  
+  //Calcular a Densidade Populacional = População / Área 
+  densidade2 = populacao2 / area2;  
 
-//Calcular Pib per Capita = PIB / população
- pibpercapita2 = pib2 / populacao2; 
+  //Calcular Pib per Capita = PIB / população
+  pibpercapita2 = pib2 / populacao2; 
+
+  //Calcular o Super Poder somando todos atributos numéricos
+  float superpoder2 = 
+     (float)populacao2 + 
+     area2 + 
+     pib2 + 
+     turismo2 + 
+     pibpercapita2 + 
+     ((float) 1 / densidade2);
+
 
 
   // Área para exibição dos dados da cidade
@@ -97,9 +117,14 @@ int main() {
   printf("PIB: %.2f bilhões de reais\n" ,pib1);
   printf("Número de Pontos Turísticos: %d\n" ,turismo1);
 
+  //Imprimir resultado das novas variáveis: Densidade Populacional e Pib per Capita
   printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
   printf("PIB per Capita: %.2f \n", pibpercapita1);
 
+  //Imprimir dados do Super Poder
+  printf("O Super Poder é: %f\n", superpoder1);
+
+  
      //Imprimir os dados da Cidade 2
 
   printf("\n - Carta 2 -\n");
@@ -115,8 +140,33 @@ int main() {
   printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
   printf("PIB per Capita: %.2f \n", pibpercapita2);
 
+  //Imprimir dados do Super Poder
+  printf("O Super Poder é: %f\n", superpoder2);
+
+ //Área de comparação de atributos das Cartas
+  int resultadopopulacao = populacao1 >= populacao2;
+  int resultadoarea = area1 >= area2;
+  int resultadopib = pib1 >= pib2;
+  int resultadoturismo = turismo1 >= turismo2;
+  int resultadodensidade = densidade1 <= densidade2;
+  int resultadopibpercapita = pibpercapita1 >= pibpercapita2;
+  int resultadosuperpoder = superpoder1 >= superpoder2;
+
+
+ //Imprimir resultado da Comparação
+  printf("\n- Comparação de Cartas -\n"); 
+  printf("Carta 1 Venceu %d\n", resultadopopulacao);
+  printf("Carta 1 Venceu %d\n", resultadoarea);
+  printf("Carta 1 Venceu %d\n", resultadopib);
+  printf("Carta 1 Venceu %d\n", resultadoturismo);
+  printf("Carta 1 Venceu %d\n", resultadodensidade);
+  printf("Carta 1 Venceu %d\n", resultadopibpercapita);
+  printf("Carta 1 Venceu %d\n", resultadosuperpoder);    
+  
 
 
 
-return 0;
+
+ return 0;
 } 
+
